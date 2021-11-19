4f98431232d20f8cd1bb8865ec05fcbc393bdb53
carlos david <cdordonezs2408@gmail.com>
2021-11-19 15:19:32 -0500

    dark mode added

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,15 @@
 function contacting() {
   alert("I will get in contact whit you as soon as posible");
 }
+
+document.getElementById("id-sun").onclick = function () {
+  document.getElementById("document").classList.remove("darkmode");
+  document.getElementById("id-moon").classList.remove("active");
+  document.getElementById("id-sun").classList.add("active");
+};
+
+document.getElementById("id-moon").onclick = function () {
+  document.getElementById("document").classList.add("darkmode");
+  document.getElementById("id-sun").classList.remove("active");
+  document.getElementById("id-moon").classList.add("active");
+};
